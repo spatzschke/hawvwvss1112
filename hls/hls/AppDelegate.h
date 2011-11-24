06d25a5263decfc514bd57556723f7f4b4d03f39
Stan Patzschke <stan.patzschke@googlemail.com>
2011-11-24 16:24:26 +0100

    xcode init

---
+++ hls/hls/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  hls
+//
+//  Created by Stan Patzschke on 24.11.11.
+//  Copyright (c) 2011 commercetools.de. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
