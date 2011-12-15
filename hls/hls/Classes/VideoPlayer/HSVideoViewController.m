dd3a3d103279d4e3ab59305e774e6f695e42cca4
Jennifer Schöndorf <Jenny.88@web.de>
2011-12-15 14:28:11 +0100

    icons fullscreen toggle

--- hls/hls/Classes/VideoPlayer/HSVideoViewController.m
+++ hls/hls/Classes/VideoPlayer/HSVideoViewController.m
@@ -490,10 +490,12 @@ static NSString *timeStringForSeconds(Float64 seconds)
 {
     UIImage *buttonImage;
     
+    NSLog(@"Hallo");
     if (isFullscreen)
         buttonImage = [UIImage imageNamed:@"fullscreen-exit"];
     else
-        buttonImage = [UIImage imageNamed:@"fullscreen-exit"];
+        buttonImage = [UIImage imageNamed:@"fullscreen-enter"];
+    
     
     [fullscreenButton setImage:buttonImage forState:UIControlStateNormal];
 }
